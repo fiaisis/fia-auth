9e3b9be21c3c611e519e8f11da17dcd93d1ca86f
Keiran Price <keiran.price@stfc.ac.uk>
2024-05-29 07:58:05 +0100

    Add exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,14 @@
+class UOWSError(Exception):
+    """Problem authenticating with the user office web service"""
+
+
+class ProposalAllocationsError(Exception):
+    """Problem connecting with the proposal allocations api"""
+
+
+class BadCredentialsError(Exception):
+    """ "Bad Credentials Provided"""
+
+
+class BadJWTError(Exception):
+    """Raised when a bad jwt has been given to the service"""
