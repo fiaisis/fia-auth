9c5086d224c5e7ff1dea20a9e2de61e47ba10e15
Keiran Price <keiran.price@stfc.ac.uk>
2024-05-29 07:55:58 +0100

    Add initial app

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+ALLOWED_ORIGINS = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
