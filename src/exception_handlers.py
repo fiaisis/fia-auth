c63c6967a19130176c5b7b331e08b70bdf98db5e
keiranjprice101 <44777678+keiranjprice101@users.noreply.github.com>
2024-06-13 10:00:15 +0100

    Update src/exception_handlers.py
    Co-authored-by: Samuel Jones <samjones714@gmail.com>

--- src/exception_handlers.py
+++ src/exception_handlers.py
@@ -7,7 +7,7 @@ async def auth_error_handler(_: Request, __: Exception) -> JSONResponse:
     Automatically return a 403 when an authentication error is raised
     :param _:
     :param __:
-    :return: JSONResponse with 404
+    :return: JSONResponse with 403
     """
     return JSONResponse(
         status_code=HTTPStatus.FORBIDDEN,
