77e2871264d37c05a6a99950d6e0d1db4a56432a
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-20 14:15:23 +0100

    Create table in test env

---
+++ test/e2e/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src.db import ENGINE, Base
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _setup():
+    """
+    Setup database pre-testing
+    :return:
+    """
+    Base.metadata.drop_all(ENGINE)
+    Base.metadata.create_all(ENGINE)
