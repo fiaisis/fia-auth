f615c4c8ce62182e326ae1f50182ec37458050c8
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-28 08:45:49 +0100

    Add more logging

--- fia_auth/exception_handlers.py
+++ fia_auth/exception_handlers.py
@@ -1,16 +1,20 @@
+import logging
 from http import HTTPStatus
 
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+logger = logging.getLogger(__name__)
 
-async def auth_error_handler(_: Request, __: Exception) -> JSONResponse:
+
+async def auth_error_handler(_: Request, exc: Exception) -> JSONResponse:
     """
     Automatically return a 403 when an authentication error is raised
     :param _:
-    :param __:
+    :param exc: The caught exception
     :return: JSONResponse with 403
     """
+    logger.info(f"Exception was caught {exc}")
     return JSONResponse(
         status_code=HTTPStatus.FORBIDDEN,
         content={"message": "Forbidden"},
