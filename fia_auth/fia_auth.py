62cdf5745b1ee6da007f33f49166bbdb5d436345
Samuel <47388695+Dagonite@users.noreply.github.com>
2025-11-14 13:53:45 +0000

    Update auth endpoints and response formats (#160)
    * Update auth endpoints and response formats
    
    * Remove unused REFRESH_COOKIE_PATH var
    
    * Update logging
    
    * Formatting and linting commit
    
    ---------
    
    Co-authored-by: Keiran Price <keiran.price@stfc.ac.uk>
    Co-authored-by: github-actions <github-actions@github.com>

--- fia_auth/fia_auth.py
+++ fia_auth/fia_auth.py
@@ -1,5 +1,8 @@
 """Module containing the fast api app. Uvicorn loads this to start the api"""
 
+import logging
+import sys
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -7,7 +10,17 @@ from fia_auth.exception_handlers import auth_error_handler
 from fia_auth.exceptions import AuthenticationError
 from fia_auth.routers import ROUTER
 
+file_handler = logging.FileHandler(filename="run-detection.log")
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+logging.basicConfig(
+    handlers=[file_handler, stdout_handler],
+    format="[%(asctime)s]-%(name)s-%(levelname)s: %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
 ALLOWED_ORIGINS = ["*"]
+
 
 app = FastAPI()
 app.add_middleware(
